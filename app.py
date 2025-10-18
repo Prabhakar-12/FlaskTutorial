6b0b066b1a3a7bcb627d78e398a4dfda46e8afc4
Prabhakar <prabhakarkondakalla9@gmail.com>
2025-10-18 20:54:40 +0530

    ,,

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-# import datetime
 #
 # #importing the Flask class from flask module
 # from flask import Flask,render_template
-# import datetime
-# #Creating the Flask Application
 # app=Flask(__name__)
-# #Defining the Route URL path
 # @app.route('/')
-# def home():
+# def Home():
 #     return render_template('index.html')
-# #Another Route
 # @app.route('/about')
-# def about():
+# def About():
 #     return render_template('about.html')
-# #Another Route
 # @app.route('/sample')
-# def sample():
+# def Sample():
 #     return render_template('sample.html')
-# #Run the app
+# @app.route('/practice')
+# def practice():
+#     return render_template('practice.html')
 # if __name__=="__main__":
 #     app.run()
-#importing the Flask class from flask module
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route('/')
-def Home():
-    return render_template('index.html')
-@app.route('/about')
-def About():
-    return render_template('about.html')
-@app.route('/sample')
-def Sample():
-    return render_template('sample.html')
-if __name__=="__main__":
-    app.run()
