9068335919fed2722eada076a9cc79493ff50467
Prabhakar <prabhakarkondakalla9@gmail.com>
2025-10-19 12:07:50 +0530

    ..

---
+++ sample.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route('/')
+def Home():
+    return render_template('a.html')
+@app.route('/about')
+def About():
+    return render_template('b.html')
+if __name__=="__main__":
+    app.run()
