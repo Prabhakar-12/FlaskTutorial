3912c33a7f30759d7a8d12b3af9dc5bb80f69eec
Prabhakar <prabhakarkondakalla9@gmail.com>
2025-10-19 13:17:37 +0530

    kk..

--- sample.py
+++ sample.py
@@ -5,6 +5,11 @@ def Home():
     return render_template('a.html')
 @app.route('/about')
 def About():
-    return render_template('b.html')
+    items=[
+        {'id':1,'name':'phone','barcode':'76355363536','price':500},
+        {'id':2,'name':'Laptop','barcode':'2976378633','price':300},
+        {'id':3,'name':'keyboard','barcode':'37635763','price':600}
+    ]
+    return render_template('b.html',items=items)
 if __name__=="__main__":
     app.run()
